148a07f6c0894c989ce8ff82add4e357f7ab936c
llucasmota <lucas.o.mota@gmail.com>
2020-01-29 08:34:32 -0300

    Config inicial do sentry

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import routes from './app/routes';
 import Database from './database';
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
 
 const app = express();
 
+Sentry.init(sentryConfig);
+app.use(Sentry.Handlers.requestHandler());
+
 app.use(express.json());
 app.use(routes);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(3333);
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://f4c7d3c268cf4cdc81766e883d37d3c2@sentry.io/2064907',
+};
